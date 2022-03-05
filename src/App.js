00e467498a9d2f4541101e67fbb253bd8452e2c7
Vinay <itsvinayadav@gmail.com>
2022-03-05 14:20:30 +0530

    Added login and redirect logged user functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Login from './components/Login'
 import Header from './components/Header'
+import Home from './components/Home';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <Login />
+          </Route>
+          <Route path='/home'>
+            <Home />
           </Route>
         </Switch>
       </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,80 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { auth, provider } from "../firebase";
+import { selectUserName, selectUserPhoto, setUserLoginDetails } from '../features/user/userSlice';
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userName = useSelector(selectUserName)
+  const userPhoto = useSelector(selectUserPhoto)
 
-    const handleAuth = () => {
-        auth.signInWithPopup(provider).then((result) => {
-            console.log(result);
-        }).catch((error) => {
-            alert(error.message);
-        })
-    }
-    return (
-        <Nav>
-            <Logo>
-                <img src="/images/logo.svg" alt='Disney+' />
-            </Logo>
-            <NavMenu>
-                <a href="/home">
-                    <img src="/images/home-icon.svg" alt="HOME" />
-                    <span>HOME</span>
-                </a>
-                <a>
-                    <img src="/images/search-icon.svg" alt="SEARCH" />
-                    <span>SEARCH</span>
-                </a>
-                <a>
-                    <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-                    <span>WATCHLIST</span>
-                </a>
-                <a>
-                    <img src="/images/original-icon.svg" alt="ORIGINALS" />
-                    <span>ORIGINALS</span>
-                </a>
-                <a>
-                    <img src="/images/movie-icon.svg" alt="MOVIES" />
-                    <span>MOVIES</span>
-                </a>
-                <a>
-                    <img src="/images/series-icon.svg" alt="SERIES" />
-                    <span>SERIES</span>
-                </a>
-            </NavMenu>
-            <Login onClick={handleAuth}>Login</Login>
-        </Nav>
-    )
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push('/home')
+      }
+    })
+  }, [userName])
+
+
+  const handleAuth = () => {
+    auth.signInWithPopup(provider).then((result) => {
+      setUser(result.user)
+    }).catch((error) => {
+      alert(error.message);
+    })
+  }
+
+  const setUser = (user) => {
+    dispatch(setUserLoginDetails({
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL
+    }))
+  }
+
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt='Disney+' />
+      </Logo>
+      {!userName ? <Login onClick={handleAuth}>Login</Login> :
+        <>
+          <NavMenu>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="HOME" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="SEARCH" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="ORIGINALS" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="MOVIES" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="SERIES" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+          <UserImg src={userPhoto} alt={userName} />
+        </>
+      }
+    </Nav>
+  )
 }
 
 const Nav = styled.nav`
